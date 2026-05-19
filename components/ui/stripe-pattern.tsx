e1745b09ce2ce0737730a56d65cab69e786273ab
hameed <mohamedishaq499@gmail.com>
2026-05-19 12:08:49 +0530

    resposiveness

--- components/ui/stripe-pattern.tsx
+++ components/ui/stripe-pattern.tsx
@@ -1,6 +1,6 @@
 export default function StripePattern() {
   return (
     <div
-      className="ml-0 sm:ml-[5px] md:ml-[5px] lg:h-[74px] lg:mt-[30px] h-[64px] mb-[28px] w-full sm:w-[calc(100%-20px)] md:w-[calc(100%-44px)] lg:w-[calc(100%-32px)] overflow-hidden rounded-[8px] bg-[repeating-linear-gradient(-45deg,#E8BE42_0px,#E8BE42_2px,transparent_2px,transparent_14px)]" />
+      className="mx-auto w-full max-w-350 px-6 md:px-0 h-[64px] lg:h-[74px] lg:mt-[30px] mb-[28px] overflow-hidden rounded-[8px] bg-[repeating-linear-gradient(-45deg,#E8BE42_0px,#E8BE42_2px,transparent_2px,transparent_14px)]" />
   );
 }
